22bc61ead121abe4e797e515bf9a7d47fd4d5d86
Victor Leão <leaovic@gmail.com>
2021-10-27 22:44:44 -0300

    Exercicio de Mesa concluido

---
+++ ProgramacaoImperativa/Aula06/calculadora.js
@@ -0,0 +1,49 @@
+
+function somar(a,b) {
+    return a + b;
+}
+
+console.log(somar(10,10));
+
+function subtrair(a,b) {
+    return a - b;
+}
+
+console.log(subtrair(10,10));
+
+function multiplicar(a,b) {
+    return a * b;
+}
+
+console.log(multiplicar(1,0));
+
+function dividir(a,b) {
+    return a / b;
+}
+
+console.log(dividir(10,0))
+
+function quadradoDoNumero(a,b) {
+    return a * b;
+}
+
+console.log(quadradoDoNumero(2,2))
+
+function mediaDeTresNumeros(a,b,c) {
+    return (a+b+c)/3;
+}
+
+console.log(mediaDeTresNumeros(2,2,2))
+
+function calculaPorcentagem(a,b) {
+    return a*(b/100);
+}
+
+console.log(calculaPorcentagem(1000,10))
+
+function geradorDePorcentagem(a,b) {
+    return (a*100)/b;
+}
+
+console.log(geradorDePorcentagem(2,200))
+
