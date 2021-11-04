87c1b9540b39799d55cff509f0c2e0841814a4fc
Victor Leão <leaovic@gmail.com>
2021-11-03 21:57:49 -0300

    Exercicio de Mesa LP07 concluido

---
+++ ProgramacaoImperativa/Aula07/Mesa/Mesa07.js
@@ -0,0 +1,46 @@
+// Ex1. O que cada expressão retorna?
+
+// console.log(!true);
+
+// console.log(!false);
+
+// console.log(!!false);
+
+// console.log(!!true);
+
+// console.log(!1);
+
+// console.log(!!1);
+
+// console.log(!0);
+
+// console.log(!!0);
+
+// console.log(!!"");
+
+// let x=5;
+// let y=9;
+
+// console.log(x<10&&x!==5);
+// console.log(x>9||x===5);
+// console.log(!(x===y));
+
+
+// Ex2. O que cada expressão retorna?
+
+// let x=10;
+// let y="a";
+
+// console.log(y==="b"||x>=10);
+
+// let x=3;
+// let y=8;
+
+// console.log(!(x=="3"||x===y)&&!(y!==8&&x<=y));
+
+// let str ="";
+// let msg ="haha!";
+// let eBonito="false";
+
+// console.log(!((str||msg)&&eBonito));
+
