05a9509ee6dd186f4a29b90a0f171ebaaf423478
Victor Leão <leaovic@gmail.com>
2021-11-03 21:58:42 -0300

    Aula 07

---
+++ ProgramacaoImperativa/Aula07/Aula07.js
@@ -0,0 +1,13 @@
+// console.log( (10 > 20) );
+
+// console.log( (10 > 20) );
+
+// console.log( (20 == 10) );
+
+// console.log((20==10) || (20!=10));
+
+// console.log((20==10) && (20!=10));
+
+// console.log(false || true);
+
+console.log(!false);
