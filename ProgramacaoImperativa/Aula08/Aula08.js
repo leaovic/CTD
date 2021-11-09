c3fc8245262a80225c6561ef30cebe605468ad87
Victor Leão <leaovic@gmail.com>
2021-11-09 00:10:56 -0300

    Aula08 e Mesa08

---
+++ ProgramacaoImperativa/Aula08/Aula08.js
@@ -0,0 +1,27 @@
+
+let idade=15;
+
+
+if(idade>18){
+    console.log("Voce e maior de idade");
+}
+
+else if(idade==18){
+    console.log("Esta aprendendo");
+}
+else{
+    console.log("Voce nao e maior de idade");
+}
+
+switch(idade){
+    case 18:
+        console.log("idade e igual a 18");
+        break;
+    case 19:
+        console.log("idade e igual a 19");
+        break;
+    default:
+        console.log("nenhum deles");
+    
+}
+
---
+++ ProgramacaoImperativa/Aula08/Mesa08.js
@@ -0,0 +1,29 @@
+
+
+// function podeSubir(altura, responsavel) {
+//     if (altura >= 1.40 && altura <= 2.0){
+//     return true;
+//     }
+//     else if ((altura <= 1.40 && altura >= 1.20) && responsavel == true){
+//     return true;
+//     }
+//     else {
+//         return false
+//     }
+// }
+
+console.log(podeSubir(3, true));
+
+function podeSubir(altura, responsavel) {
+    if (altura >= 1.40 && altura <= 2.0){
+    console.log("Acesso autorizado");
+    }
+    else if ((altura <= 1.40 && altura >= 1.20) && responsavel == true){
+    console.log("Acesso autorizado somente acompanhado por pessoa responsavel");
+    }
+    else {
+        console.log("Acesso negado");
+    }
+}
+
+podeSubir(1.80, false)
