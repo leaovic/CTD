2982c666e4cd837d637acb7b699d054c5a8daecc
Victor Leão <leaovic@gmail.com>
2021-11-22 21:03:02 -0300

    Aula13 e mesa de front

---
+++ ProgramacaoImperativa/Aula12/aula12.js
@@ -0,0 +1,19 @@
+const alicia = [30, 30, 30];
+const bob = [30,30, 48];
+
+console.log(findWinner(alicia, bob));
+
+function findWinner(a=[], b=[]){
+    let aliciaPoints =0;
+    let bobPoints =0;
+    for(let i =0; i < a.length; i++){
+        if(a[i] > b[i]){
+            aliciaPoints++;
+        }
+        else if(a[i] < b[i]){
+            bobPoints++;
+        }else{
+        }
+    }
+    return aliciaPoints > bobPoints ? `Alicia ganhou com ${aliciaPoints} pontos`: `Bob ganhou com ${bobPoints} pontos`;
+}
