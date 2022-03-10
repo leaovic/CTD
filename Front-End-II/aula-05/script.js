c208c774b75f770f5c3f71c45bcef0e26dccb2d7
Victor Leão <leaovic@gmail.com>
2022-03-10 19:26:51 -0300

    atualizando

---
+++ Front-End-II/aula-05/script.js
@@ -0,0 +1,23 @@
+// let nomeUsuario = "Paulo"
+
+// console.log(`Olá, usuario ${nomeUsuario}`);
+
+// function nomeUsuario(){
+//     return nomeUsuario;
+// }
+
+// let h1 = document.querySelector('h1');
+
+
+// h1.innerHTML = "<h6>Novo titulo</h6>";
+
+
+// let body = document.querySelector('h1');
+
+// body.style.backgroudColor = "Black";
+
+// let h4 = document.createElement('h4');
+
+// h4.innerHTML = "Titulo 44444"
+// console.log(h4);
+
